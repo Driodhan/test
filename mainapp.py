ce386a9ee1f72412340b07ef928917ec662e7a2d
Driodhan <108714579+Driodhan@users.noreply.github.com>
2024-01-09 13:25:29 +0500

    Update mainapp.py

--- mainapp.py
+++ mainapp.py
@@ -5,6 +5,5 @@ st.write(' driodhan ')
 st.write(' 20sw45 ')
 st.write(' software Engineering ')
 st.write(' QUEST NAWABSHAH ')
-st.thislist = ["apple", "banana", "cherry", "apple", "cherry"]
-print(thislist)
+
 
