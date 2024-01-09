d0e2290f581eec17dfa757ce62719fab6db615fa
Driodhan <108714579+Driodhan@users.noreply.github.com>
2024-01-09 13:15:04 +0500

    Update mainapp.py

--- mainapp.py
+++ mainapp.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
 
-st.write(' here driodhan ')
-print('driodhan')
+st.write(' driodhan ')
+st.write(' 20sw45 ')
+st.write(' software Engineering ')
+st.write(' QUEST NAWABSHAH ')
+
